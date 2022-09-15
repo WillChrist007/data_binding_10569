e0069f4cca829e8fe790765d53a4766f715e8042
WillChrist007 <williamchristians007@gmail.com>
2022-09-15 07:55:38 +0700

    Initial commit

---
+++ app/src/main/java/com/william/data_binding_10569/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.william.data_binding_10569
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.william.data_binding_10569.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,R.layout.activity_main
+        ).apply {
+            this.lifecycleOwner = this@MainActivity
+            this.viewmodel = mainViewModel
+        }
+
+        mainViewModel.editTextContent.observe(this){
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
